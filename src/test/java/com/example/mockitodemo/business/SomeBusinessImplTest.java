dd810db4d2f2c556dba2d3f0a561fb2fd6ae03f0
Farhad <fgadirli@turanbank.az>
2022-12-21 11:21:42 +0400

    Stubs

---
+++ src/test/java/com/example/mockitodemo/business/SomeBusinessImplTest.java
@@ -0,0 +1,25 @@
+package com.example.mockitodemo.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SomeBusinessImplTest {
+
+    @Test
+    void findTheGreatestFromAllData_basic() {
+        SomeBusinessImpl business = new SomeBusinessImpl(new DataServiceStub());
+        assertEquals(25, business.findTheGreatestFromAllData());
+    }
+
+}
+
+class DataServiceStub implements DataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{25, 15, 5};
+    }
+}
