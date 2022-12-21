b3a17747d230b6a72dee6ec0cdfc6ca2e6ec6eb8
Farhad <fgadirli@turanbank.az>
2022-12-21 12:39:41 +0400

    Simplifying Tests with Mockito Annotations - @Mock, @InjectMocks

--- src/test/java/com/example/mockitodemo/business/SomeBusinessImplMockTest.java
+++ src/test/java/com/example/mockitodemo/business/SomeBusinessImplMockTest.java
@@ -1,6 +1,10 @@
 package com.example.mockitodemo.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,20 +14,28 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class SomeBusinessImplMockTest {
 
+    @Mock
+    private DataService dataServiceMock;
+
+    @InjectMocks
+    private SomeBusinessImpl business;
+
     @Test
     void findTheGreatestFromAllData_basic() {
-        DataService dataServiceMock = mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{25, 15, 5});
-        SomeBusinessImpl business = new SomeBusinessImpl(dataServiceMock);
         assertEquals(25, business.findTheGreatestFromAllData());
     }
 
     @Test
     void findTheGreatestFromAllData_oneValue() {
-        DataService dataServiceMock = mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{35});
-        SomeBusinessImpl business = new SomeBusinessImpl(dataServiceMock);
         assertEquals(35, business.findTheGreatestFromAllData());
+    }
+
+    @Test
+    void findTheGreatestFromAllData_emptyArray() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{});
+        assertEquals(Integer.MIN_VALUE, business.findTheGreatestFromAllData());
     }
 
 }
